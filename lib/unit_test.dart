ae22d98d0ed326d99935feab823ae0063a47cfac
Mehedi Hasan <mehedi@augnitive.com>
2021-10-02 12:26:07 +0600

    Function

---
+++ lib/unit_test.dart
@@ -0,0 +1,51 @@
+import 'package:test_project/function.dart';
+
+void main() {
+  
+// print(addTwo());
+
+ print(findEvenOdd(6, 3));
+  
+}
+
+
+
+//Write a function division two number 
+
+
+
+// findEvenOdd(int a) {
+
+//   if(a == 0 || a < 0 ) {
+//     return "$a is not allowed";
+//   }
+
+//   if (a % 2 == 0) {
+//     return "$a number is even";
+//   }else {
+//     return "$a number is odd";
+//   }
+// }
+
+
+//Write a function find even or odd number  
+
+addTwo() {
+  
+  int a = 10;
+  int b = 20;
+  int sum = a + b;
+//   print(sum);
+  
+//   return a + b;
+  
+  return sum;
+  
+  int num = 60;
+  print(num);
+  
+//   return sum;
+  
+}
+  
+  
