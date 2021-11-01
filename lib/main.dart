6f8765430075bdd003e808015bf84e9d7fb736a8
Mehedi Hasan <mehedi@augnitive.com>
2021-11-01 10:01:45 +0600

    Theme data

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,53 @@ void main() {
       debugShowCheckedModeBanner: false,
       title: "Demo Project",
       initialRoute: ScreenOne.path,
+      theme: ThemeData(
+        primaryColor: Colors.orange,
+        fontFamily: "Lobster",
+        brightness: Brightness.light,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.teal,
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.pink
+        ),
+        textTheme: TextTheme(
+          headline1: TextStyle(color: Colors.green, fontSize: 28),
+          headline6: TextStyle(color: Colors.green, fontSize: 12),
+          bodyText1: TextStyle(color: Colors.pink, fontSize: 20)
+        ),
+      
+        iconTheme: IconThemeData(
+          color: Colors.grey
+        ),
+        buttonColor: Colors.yellow
+
+      ),
+
+      darkTheme: ThemeData(
+        primaryColor: Colors.black,
+        fontFamily: "Lobster",
+        brightness: Brightness.dark,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black,
+        ),
+        textTheme: TextTheme(
+          headline1: TextStyle(color: Colors.white, fontSize: 28),
+          headline6: TextStyle(color: Colors.white, fontSize: 12),
+          bodyText1: TextStyle(color: Colors.white, fontSize: 20)
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.white
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.black
+        ),
+        buttonColor: Colors.yellow
+      ),
+
+      themeMode: ThemeMode.dark,
+
+
       routes: {
         ScreenOne.path :(context)=> ScreenOne(),
         ScreenTwo.path :(context)=> ScreenTwo(),
