68edf91e092dd154b422939cbbd2a34ce7629af3
Mehedi Hasan <mehedi@augnitive.com>
2021-11-22 12:44:15 +0600

    bdfoodrecipes app init

---
+++ bdfoodrecipe/lib/screen/non_veg/non_veg.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NonVeg extends StatelessWidget {
+  const NonVeg({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
