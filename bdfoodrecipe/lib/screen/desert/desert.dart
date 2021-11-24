038df983cb18f1febb8cad4c5da390f83c6c5ce9
Mehedi Hasan <mehedi@augnitive.com>
2021-11-24 10:33:12 +0600

    Code refactore

--- bdfoodrecipe/lib/screen/desert/desert.dart
+++ bdfoodrecipe/lib/screen/desert/desert.dart
@@ -1,12 +1,24 @@
+import 'package:bdfoodrecipe/data.dart';
+import 'package:bdfoodrecipe/global/functions.dart';
+import 'package:bdfoodrecipe/widget/custom_grid_tile.dart';
 import 'package:flutter/material.dart';
 
 class Desert extends StatelessWidget {
-  const Desert({ Key? key }) : super(key: key);
+  final List dessert = bdfood["dessert"];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return GridView.builder(
+      gridDelegate: customGridDelegate(), 
+      itemCount: dessert.length,
+      padding: EdgeInsets.all(15),
+      itemBuilder: (BuildContext context, int index){
+        return CustomGridTile(
+          imageUrl: dessert[index]["image"],
+          title: dessert[index]["title"],
+          totalIngredients: countIngredients(dessert[index]["ingredients"]),
+        );
+      }
     );
   }
 }
--- bdfoodrecipe/lib/screen/rice/rice.dart
+++ bdfoodrecipe/lib/screen/rice/rice.dart
@@ -1,70 +1,27 @@
 import 'package:bdfoodrecipe/data.dart';
+import 'package:bdfoodrecipe/global/functions.dart';
+import 'package:bdfoodrecipe/widget/custom_grid_tile.dart';
 import 'package:flutter/material.dart';
 
 class Rice extends StatelessWidget {
   final List rice = bdfood["rice"];
 
-  int countIngredients(List ingredients){
-    return ingredients.length;
-  }
+
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 300,
-        childAspectRatio: 8 / 7,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10
-      ), 
+      gridDelegate: customGridDelegate(), 
       itemCount: rice.length,
       padding: EdgeInsets.all(15),
       itemBuilder: (BuildContext context, int index){
-        return Card(
-          elevation: 10,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Image.asset(rice[index]["image"]),
-              SizedBox(height: 8,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: Text(
-                  rice[index]["title"],
-                  style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 20
-                  ),
-                ),
-              ),
-              
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(Icons.watch, color: Colors.red,),
-                        Text("30 min")
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.food_bank, color: Colors.red,),
-                        Text("${countIngredients(rice[index]["ingredients"])} ingredients")
-                      ],
-                    )
-
-                  ],
-                ),
-              )
-
-            ],
-          ),
+        return CustomGridTile(
+          imageUrl: rice[index]["image"],
+          title: rice[index]["title"],
+          totalIngredients: countIngredients(rice[index]["ingredients"]),
         );
       }
     );
   }
 }
+
